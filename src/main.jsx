571c7e14bb4e11be4ade9203ace46a7a09dc5f1f
aymanecoder <aymanbiti2@gmail.com>
2023-02-18 00:09:38 +0100

    end

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import './index.css'
 import { RouterProvider , createBrowserRouter} from 'react-router-dom';
 const router = createBrowserRouter([
   {path: '/',element:<App />}
-]);
+]); 
+
+//you can add a single folder for routes
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
